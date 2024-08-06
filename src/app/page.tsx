a06b1648c6ca4b6ba4e80ddf1dea7f52501762d9
Angelo Madara <angelomadara.dev@outlook.com>
2024-08-06 08:20:25 +0800

    update main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>This is a test website</h1>
+      <h1>This is a test website.</h1>
     </div>
   );
 }
