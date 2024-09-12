743c216bed030b9b33880fc3ba2c189a7161c6f8
angelo madara <angelo.madara@aiqueinnovation.com>
2024-09-12 09:40:08 +0800

    convert 14 to roman numerial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-dvh">
-      <h1 className="text-9xl font-sans font-black antialiased normal-nums">14</h1>
+      <h1 className="text-9xl font-sans font-black antialiased normal-nums">XIV</h1>
     </div>
   );
 }
