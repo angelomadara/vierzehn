abd3f27050c3fa27c83775af898643080be953e3
angelo madara <angelo.madara@aiqueinnovation.com>
2024-09-12 09:31:46 +0800

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>This is a test website.</h1>
+    <div className="flex justify-center items-center h-dvh">
+      <h1 className="text-9xl font-sans font-black antialiased normal-nums">14</h1>
     </div>
   );
 }
