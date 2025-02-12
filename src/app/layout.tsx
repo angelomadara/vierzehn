03d2b84599eaa41bdc8e4fb79c2d11ece67bb73e
Angelo Madara <angelomadara.dev@outlook.com>
2025-02-12 16:00:03 +0800

    upgrade npm packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,40 +5,27 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Oleg Madara",
-  description: "Dev Portfolio",
+    title: "Oleg Madara",
+    description: "Dev Portfolio",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="all" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className={inter.className}>{children}</body>
-
-      <script type="text/javascript">
-      var sc_project=13086563; 
-      var sc_invisible=1; 
-      var sc_security="acc6d673"; 
-      </script>
-      <script type="text/javascript"
-      src="https://www.statcounter.com/counter/counter.js"
-      async></script>
-      <noscript><div class="statcounter"><a title="Web Analytics"
-      href="https://statcounter.com/" target="_blank"><img
-      class="statcounter"
-      src="https://c.statcounter.com/13086563/0/acc6d673/1/"
-      alt="Web Analytics"
-      referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
-      
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta name="robots" content="all" />
+                <link rel="icon" href="/favicon.ico" />
+            </head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
